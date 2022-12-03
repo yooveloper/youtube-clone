eef88b1791a0b79f045c89cdf4b5d21cf6f67774
yooveloper <yooveloper@gmail.com>
2022-12-03 21:58:11 +0900

    add: youtube api 추가

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import './App.css';
 import { Outlet } from 'react-router-dom';
 import SearchHeader from './components/SearchHeader';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { YoutubeApiProvider } from './context/YoutubeApiContext';
 
 function App() {
   const queryClient = new QueryClient();
   return (
     <>
       <SearchHeader />
-      <QueryClientProvider client={queryClient}>
-        <Outlet />
-      </QueryClientProvider>
+      <YoutubeApiProvider>
+        <QueryClientProvider client={queryClient}>
+          <Outlet />
+        </QueryClientProvider>
+      </YoutubeApiProvider>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
 import NotFound from './pages/NotFound';
 import Videos from './pages/Videos';
 import VideoDetail from './pages/VideoDetail';
@@ -21,7 +21,6 @@ const router = createBrowserRouter([
     ],
   },
 ]);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
