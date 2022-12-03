541fa341eee01b8165b06c3917fef07ef969f8db
yooveloper <yooveloper@gmail.com>
2022-12-03 22:34:49 +0900

    add: 상세화면 추가

---
+++ src/pages/VideoDetail.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import ChannelInfo from '../components/ChannelInfo';
+import RelatedVideos from '../components/RelatedVideos';
+
+export default function VideoDetail() {
+  const {
+    state: { video },
+  } = useLocation();
+  const { title, channelId, channelTitle, description } = video.snippet;
+  return (
+    <section>
+      <article>
+        <iframe
+          title={video.id}
+          id='player'
+          type='text/html'
+          width='100%'
+          height='640'
+          src={`http://www.youtube.com/embed/${video.id}`}
+          frameBorder='0'
+        ></iframe>
+        <div>
+          <h2>{title}</h2>
+          <ChannelInfo id={channelId} name={channelTitle} />
+          <pre>{description}</pre>
+        </div>
+      </article>
+      <section>
+        <RelatedVideos id={video.id} />
+      </section>
+    </section>
+  );
+}
