b7fc6eae426c4ffe00f7e01990b7015d269d5c57
yooveloper <yooveloper@gmail.com>
2022-12-03 22:34:39 +0900

    add: 메인화면 반응형 구현

--- src/pages/Videos.js
+++ src/pages/Videos.js
@@ -2,18 +2,12 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import VideoCard from '../components/VideoCard';
+import { useYoutubeApi } from '../context/YoutubeApiContext';
 
 export default function Videos() {
   const { keyword } = useParams();
-  const {
-    isLoading,
-    error,
-    data: videos,
-  } = useQuery(['videos', keyword], async () => {
-    return fetch(`/videos/${keyword ? 'search' : 'popular'}.json`)
-      .then((res) => res.json())
-      .then((data) => data.items);
-  });
+  const { youtube } = useYoutubeApi();
+  const { isLoading, error, data: videos } = useQuery(['videos', keyword], () => youtube.search(keyword));
 
   return (
     <>
@@ -21,7 +15,7 @@ export default function Videos() {
       {isLoading && <p>Loading...</p>}
       {error && <p>Something is wrong 💩</p>}
       {videos && (
-        <ul>
+        <ul className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 gap-y-4'>
           {videos.map((video) => (
             <VideoCard key={video.id} video={video} />
           ))}
