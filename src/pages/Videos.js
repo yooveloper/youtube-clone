3c9a027d0b20c4c0da24d1b3be344630185c7063
yooveloper <yooveloper@gmail.com>
2022-12-03 20:32:53 +0900

    add: videos 컴포넌트 react-query 추가

---
+++ src/pages/Videos.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import VideoCard from '../components/VideoCard';
+
+export default function Videos() {
+  const { keyword } = useParams();
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery(['videos', keyword], async () => {
+    return fetch(`/videos/${keyword ? 'search' : 'popular'}.json`)
+      .then((res) => res.json())
+      .then((data) => data.items);
+  });
+
+  return (
+    <>
+      <div>Videos{keyword ? `${keyword}` : '🔥'}</div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something is wrong 💩</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
+}
