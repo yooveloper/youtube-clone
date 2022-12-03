ddecac153f9ade7c2e90e935f7ec6a54b781c887
yooveloper <yooveloper@gmail.com>
2022-12-03 20:31:30 +0900

    add: tailwindcss 추가

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        brand: '#FF0000',
+      },
+    },
+  },
+  plugins: [],
+};
