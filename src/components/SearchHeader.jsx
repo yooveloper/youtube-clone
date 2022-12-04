3b4ef7e70bd7d96266458dff91177e84f6a18ed3
yooveloper <yooveloper@gmail.com>
2022-12-04 16:30:51 +0900

    fix: 스타일 수정

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { BsYoutube, BsSearch } from 'react-icons/bs';
-import { useNavigate, Link, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 export default function SearchHeader() {
   const { keyword } = useParams();
   const navigate = useNavigate();
   const [text, setText] = useState('');
-
   const handleSubmit = (e) => {
     e.preventDefault();
     navigate(`/videos/${text}`);
@@ -20,13 +19,13 @@ export default function SearchHeader() {
         <BsYoutube className='text-4xl text-brand' />
         <h1 className='font-bold ml-2 text-3xl'>Youtube</h1>
       </Link>
-      <form onSubmit={handleSubmit} className='w-full flex justify-center'>
+      <form className='w-full flex justify-center' onSubmit={handleSubmit}>
         <input
+          className='w-7/12 p-2 outline-none bg-black text-gray-50'
           type='text'
           placeholder='Search...'
           value={text}
           onChange={(e) => setText(e.target.value)}
-          className='w-7/12 p-2 outline-none bg-black text-gray-50'
         />
         <button className='bg-zinc-600 px-4'>
           <BsSearch />
